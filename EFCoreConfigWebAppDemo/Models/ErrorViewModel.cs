02888fda6b19c379ea44ba29c7e7931ca23c8c25
Chen-Yu Pao <windperson@gmail.com>
2021-01-29 12:16:50 +0800

    Add project files.

---
+++ EFCoreConfigWebAppDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFCoreConfigWebAppDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
